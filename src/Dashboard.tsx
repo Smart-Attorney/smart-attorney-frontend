f8af089d42cc72882ec80413f8095e0b7ba2dd44
Hoa Do <hoa.do.ma@gmail.com>
2023-12-06 11:22:44 -0500

    style: fix vertical alignment of text inside buttons and search bar

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -5,12 +5,15 @@ import SortBar from "./SortBar";
 function Dashboard() {
 	return (
 		<div className="flex flex-col items-center gap-5 w-[80%] mx-auto">
-			<h1 className="font-bold text-4xl pt-10 pb-5">Case Dashboard</h1>
+			<h1 className="pt-10 pb-5 text-4xl font-bold">Case Dashboard</h1>
 			<SearchBar />
-			<div className="flex flex-row items-center w-full justify-between gap-8">
+			<div className="flex flex-row items-center justify-between w-full gap-8">
 				<SortBar />
-				<button className="bg-[#D9D9D9] h-11 rounded-md w-28" type="button">
-					New Case
+				<button
+					className="bg-[#D9D9D9] h-11 rounded-md w-28 flex justify-center items-center pb-[2px]"
+					type="button"
+				>
+					<span>New Case</span>
 				</button>
 			</div>
 			<CaseFolder />
--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,13 +1,16 @@
 function SearchBar() {
 	return (
-		<div className="flex flex-row gap-8 w-full justify-between">
+		<div className="flex flex-row justify-between w-full gap-8">
 			<input
-				className="bg-[#D9D9D9] pl-6 pr-3 h-11 rounded-full placeholder-black w-full"
+				className="bg-[#D9D9D9] pb-[2px] pl-6 pr-3 h-11 rounded-full placeholder-black w-full"
 				type="search"
 				placeholder="Search..."
 			/>
-			<button className="bg-[#D9D9D9] h-11 rounded-md w-24" type="button">
-				Search
+			<button
+				className="bg-[#D9D9D9] pb-[2px] h-11 rounded-md w-24 flex justify-center items-center"
+				type="button"
+			>
+				<span>Search</span>
 			</button>
 		</div>
 	);
