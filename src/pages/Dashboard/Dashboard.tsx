e47bcc77d44f0c3dba85c20654edaf023418999a
Hoa Do <hoa.do.ma@gmail.com>
2023-12-09 02:16:48 -0500

    feat: allow users to edit name of a new case

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -5,11 +5,9 @@ import dashboardOptions from "./DashboardSortOptions";
 import { Link } from "react-router-dom";
 
 function Dashboard() {
-
-
 	return (
 		<div className="flex flex-col items-center gap-6 w-[80%] mx-auto">
-			<h1 className="pt-10 pb-5 text-4xl font-bold">Case Dashboard</h1>
+			<h1 className="mt-10 mb-5 text-4xl font-bold">Case Dashboard</h1>
 			<SearchBar />
 
 			<div className="flex flex-row items-center justify-between w-full gap-8">
@@ -18,7 +16,7 @@ function Dashboard() {
 				<Link
 					className="bg-[#D9D9D9] h-11 rounded-md min-w-[150px] flex justify-center items-center pb-[2px]"
 					type="button"
-					to={("/new-case")}
+					to={"/new-case"}
 				>
 					<span>New Case</span>
 				</Link>
--- src/pages/NewCase/NewCase.tsx
+++ src/pages/NewCase/NewCase.tsx
@@ -2,13 +2,47 @@ import SearchBar from "../../components/SearchBar";
 import SortBar from "../../components/SortBar";
 import CaseFile from "./CaseFile";
 import newCaseSortOptions from "./newCaseSortOptions";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import EditPenIcon from "../../assets/content-edit-pen.svg";
 
 function NewCase() {
+	const [editable, setEditable] = useState(false);
+	const [caseInfo, setCaseInfo] = useState({
+		name: "New Case",
+	});
+
+	console.log(caseInfo);
+
+	const toggleEditable = () => {
+		setEditable(true);
+		const caseName = document.getElementById("case-name");
+		setTimeout(() => {
+			caseName?.focus();
+		}, 100);
+	};
+
+	const handleBlur = (event: React.FocusEvent<HTMLSpanElement>) => {
+		const { textContent } = event.nativeEvent.target as HTMLElement;
+
+		setCaseInfo({ name: textContent ? textContent : "" });
+	};
 
 	return (
 		<div className="flex flex-col items-center gap-6 w-[80%] mx-auto">
-			<h1 className="pt-10 pb-5 text-4xl font-bold">New Case</h1>
+			<div className="flex flex-row items-end h-20 gap-2 mb-5">
+				<span
+					id="case-name"
+					className="relative mt-10 mb-5 text-4xl font-bold border-b border-b-black top-5"
+					contentEditable={editable}
+					suppressContentEditableWarning={true}
+					onBlur={handleBlur}
+				>
+					{caseInfo.name}
+				</span>
+
+				<img src={EditPenIcon} width="30px" className="cursor-pointer mt-7" onClick={toggleEditable} />
+			</div>
 			<SearchBar />
 
 			<div className="flex flex-row items-center justify-between w-full gap-8">
@@ -30,7 +64,7 @@ function NewCase() {
 					<Link
 						className="bg-[#D9D9D9] h-11 rounded-md min-w-[100px] flex justify-center items-center pb-[2px]"
 						type="button"
-            to={"/dashboard"}
+						to={"/dashboard"}
 					>
 						<span>Create</span>
 					</Link>
