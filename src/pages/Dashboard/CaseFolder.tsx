f74d43de179dc8641bf73bd8550fd9bcd47ed909
Hoa Do <hoa.do.ma@gmail.com>
2023-12-14 02:12:09 -0500

    feat: deadline dates properly formmated

--- src/pages/Dashboard/CaseFolder.tsx
+++ src/pages/Dashboard/CaseFolder.tsx
@@ -45,9 +45,17 @@ function CaseFolder() {
 		setCases(filteredOutItemAray);
 	};
 
+	const formatDate = (date: string) => {
+		const dateArray = date.split("-");
+		const formattedDate = `${dateArray[1]}/${dateArray[2]}/${dateArray[0]}`;
+		return formattedDate;
+	};
+
 	return (
 		<div className="grid gap-8 min-[2300px]:grid-cols-6 min-[1900px]:grid-cols-5 min-[1500px]:grid-cols-4 min-[1100px]:grid-cols-3 min-[650px]:grid-cols-2">
 			{cases?.map((caseInfo) => {
+				const formattedDate = formatDate(caseInfo.deadline);
+
 				return (
 					<div
 						className="bg-[#D9D9D9] h-64 w-64 rounded-3xl py-4 pl-8 flex flex-col justify-between"
@@ -55,7 +63,7 @@ function CaseFolder() {
 						id={caseInfo.id}
 					>
 						<div className="flex flex-row items-center gap-4 w-fit">
-							<p>Deadline: {caseInfo.deadline}</p>
+							<p>Deadline: {formattedDate}</p>
 							<div className="w-4 h-4 rounded-full" style={{ backgroundColor: `${caseInfo.status}` }}></div>
 						</div>
 						<FolderMenu
