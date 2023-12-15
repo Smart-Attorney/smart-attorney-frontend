6cb048e6cc8e3ce03820579748b90794514b0737
Hoa Do <hoa.do.ma@gmail.com>
2023-12-15 03:08:02 -0500

    style: add labels to date picker and label assigner and improve design

--- src/pages/Dashboard/FolderMenu.tsx
+++ src/pages/Dashboard/FolderMenu.tsx
@@ -46,29 +46,38 @@ function FolderMenu(props: FolderMenuProps) {
 				<span className="block w-1 h-1 bg-[#9C9DA4] m-[2px] rounded-full"></span>
 				<span className="block w-1 h-1 bg-[#9C9DA4] m-[2px] rounded-full"></span>
 			</div>
+
 			{/* Menu Options */}
 			<ul
-				className="absolute bg-[#C0C0C0] w-fit p-1 rounded-md"
+				className="absolute bg-[#eff1f3] p-1 rounded-md z-10 border border-black w-max"
 				style={{ display: isMenuOpen ? "block" : "none" }}
 				onMouseLeave={closeMenu}
 			>
-				<li className="px-1 cursor-pointer hover:bg-white hover:rounded-sm" onClick={toggleDatePicker}>
+				<li className="px-1 cursor-pointer hover:bg-[#C0C0C0] hover:rounded-sm" onClick={toggleDatePicker}>
 					Add Deadline
 				</li>
-				<li className="px-1 cursor-pointer hover:bg-white hover:rounded-sm" onClick={toggleLabelAssigner}>
+				<li
+					className="px-1 cursor-pointer hover:bg-[#C0C0C0] hover:rounded-sm"
+					onClick={toggleLabelAssigner}
+				>
 					Add Labels
 				</li>
-				<li className="px-1 cursor-pointer hover:bg-white hover:rounded-sm" onClick={deleteFolder}>
+				<li className="px-1 cursor-pointer hover:bg-[#C0C0C0] hover:rounded-sm" onClick={deleteFolder}>
 					Delete
 				</li>
 			</ul>
+
 			{/* Date Picker */}
-			<div className="absolute right-[127px]" style={{ display: isDatePickerOpen ? "block" : "none" }}>
+			<div
+				className="absolute right-[125px] top-[90px] z-10 border border-black p-3 rounded-lg bg-[#eff1f3]"
+				style={{ display: isDatePickerOpen ? "block" : "none" }}
+			>
+				<label htmlFor="date-picker">Set Deadline:</label>
 				<div className="flex flex-row items-center w-40 gap-1">
 					<input
-						className="left-[5px] rounded-sm cursor-pointer px-2"
+						className="left-[5px] rounded-sm cursor-pointer px-2 border border-black"
+						id="date-picker"
 						type="date"
-						name="deadline"
 						onChange={props.addDeadline}
 					/>
 					<img
@@ -78,14 +87,16 @@ function FolderMenu(props: FolderMenuProps) {
 					/>
 				</div>
 			</div>
+
 			{/* Label Assigner */}
 			<div
-				className="absolute right-[150px] top-[75px]"
+				className="absolute right-[125px] top-[90px] z-10 border border-black p-3 rounded-lg bg-[#eff1f3]"
 				style={{ display: isLabelAssignerOpen ? "block" : "none" }}
 			>
 				<div className="flex flex-col">
 					<form>
-						<input className="w-40 px-2 rounded-sm" type="text" />
+						<label htmlFor="labels">New Label:</label>
+						<input className="w-40 px-2 pb-[2px] border border-black rounded-sm" id="labels" type="text" />
 						<div className="flex flex-row justify-between">
 							<button
 								className="w-16 border border-black rounded-sm hover:bg-gray-400"
