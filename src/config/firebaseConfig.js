e94546759a0f4fd572e4463ae0e802900a913e2b
Hoa Do <hoa.do.ma@gmail.com>
2023-12-17 15:58:08 -0500

    feat: connect app to cloud file storage provider

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAhPuHwfbUpEjiBb7pObuhsKToyU9na2kA",
+	authDomain: "smart-attorney-564b2.firebaseapp.com",
+	projectId: "smart-attorney-564b2",
+	storageBucket: "smart-attorney-564b2.appspot.com",
+	messagingSenderId: "587221123621",
+	appId: "1:587221123621:web:b70f45e764385da9f575cc",
+	measurementId: "G-GDFTJQ0GVC",
+};
+
+export default firebaseConfig;
