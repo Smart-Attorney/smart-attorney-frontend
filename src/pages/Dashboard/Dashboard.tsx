1c216163a9003f41a79eb352d19f9851810f73b9
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2023-12-19 11:54:24 -0500

    changed background from to left : tobottom

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 function Dashboard() {
 	return (
-		<div className="dashboard-container" style={{ background: 'linear-gradient(to right, #000273, #000000)' }}>
+		<div className="dashboard-container" style={{ background: 'linear-gradient(to bottom, #000273, #000000)' }}>
 			<div className="flex flex-col items-center gap-6 w-[80%] mx-auto">
 				<h1 className="mt-10 mb-5 text-4xl font-bold" style={{ color: '#FFFFFF' }}>Case Dashboard</h1>
 				<SearchBar />
