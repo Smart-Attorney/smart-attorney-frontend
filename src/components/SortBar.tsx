f7e30afb940fe504c576a36219eb3d1e3a1d7d94
Hoa Do <hoa.do.ma@gmail.com>
2023-12-22 16:41:56 -0500

    refactor: remove unnecessary inital mapping of sort options for SortBar component

--- src/components/SortBar.tsx
+++ src/components/SortBar.tsx
@@ -7,38 +7,28 @@ interface Options {
 }
 
 function SortBar({ options }: { options: Options[] }) {
-	const optionsArray = options.map((option) => {
-		return option;
-	});
-
-	const [sortOptions, setSortOptions] = useState<Options[]>(optionsArray);
+	const [sortOptions, setSortOptions] = useState<Options[]>(options);
 
 	const handleClick = (event: React.MouseEvent<HTMLParagraphElement>) => {
 		const { id } = event.target as HTMLParagraphElement;
-
 		setSortOptions((prev) =>
-			prev.map((option) => {
-				return id === option.name ? { ...option, clicked: !option.clicked } : option;
-			})
+			prev.map((option) => (id === option.name ? { ...option, clicked: !option.clicked } : option))
 		);
 	};
 
-	const optionElements = sortOptions.map((option) => {
-		return (
-			<SortOption
-				key={option.name}
-				id={option.name}
-				name={option.name}
-				clicked={option.clicked}
-				onClick={(event) => handleClick(event)}
-			/>
-		);
-	});
+	const optionElements = sortOptions.map((option) => (
+		<SortOption
+			key={option.name}
+			id={option.name}
+			name={option.name}
+			clicked={option.clicked}
+			onClick={(event) => handleClick(event)}
+		/>
+	));
 
 	return (
 		<div className="flex flex-row flex-wrap items-center gap-6">
 			<p className="mr-6">Sort by:</p>
-
 			{optionElements}
 		</div>
 	);
