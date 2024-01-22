d6a2b8891c4e212429dfcffa06c0b204d73cb59b
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-01-22 10:37:31 -0500

    added functionality to the dashboard and case folder buttons

--- src/components/Sidebar/SidebarItems.tsx
+++ src/components/Sidebar/SidebarItems.tsx
@@ -5,39 +5,42 @@ import calendar from "../../assets/smart-attorney-figma/Calendar.png";
 import team from "../../assets/smart-attorney-figma/Team.png";
 import notification from "../../assets/smart-attorney-figma/Component 2 (1).png";
 import settings from "../../assets/smart-attorney-figma/Settings.png";
+import { useNavigate } from "react-router-dom";
 function SidebarItems() {
+	const navigate = useNavigate();
+
 	return (
 		<div>
 			<div className="flex flex-col items-center w-full h-full">
 				<img className="cursor-pointer w-20 h-20" src={SmartAttorneyLogo} />
 			</div>
 			<div className="flex flex-col items-center w-full h-full">
-				<button className ="cursor-pointer" onClick={() => { /* Handle button click */ }}>
+				<button className ="cursor-pointer" onClick={() => navigate("/dashboard")}>
 					<div className="flex flex-col items-center w-full h-full mt-8 mb-4">
 						<img className="cursor-pointer w-10 h-10" src={DashboardSym} />
 					</div>
 				</button>
-				<button onClick={() => { /* Handle button click */ }}> 
+				<button className ="cursor-pointer" onClick={() => navigate("/create-case")}> 
 					<div className="flex flex-col items-center w-full h-full mt-4 mb-4">
 						<img className="cursor-pointer w-10 h-10" src={FolderFromFigma} />
 					</div>
 				</button>
-				<button>
+				<button className ="cursor-pointer" onClick={() => { /* Handle button click */ }}>
 					<div className="flex flex-col items-center w-full h-full mt-4 mb-4">
 						<img className="cursor-pointer w-10 h-10" src={calendar} />
 					</div>
 				</button>
-				<button>
+				<button className ="cursor-pointer" onClick={() => { /* Handle button click */ }}>
 					<div className="flex flex-col items-center w-full h-full mt-4 mb-4">
 						<img className="cursor-pointer w-10 h-10" src={team} />
 					</div>
 				</button>
-				<button>
+				<button className ="cursor-pointer" onClick={() => { /* Handle button click */ }}>
 					<div className="flex flex-col items-center w-full h-full mt-4 mb-4">
 						<img className="cursor-pointer w-10 h-10" src={notification} />
 					</div>
 				</button>
-				<button>
+				<button className ="cursor-pointer" onClick={() => { /* Handle button click */ }}>
 					<div className="flex flex-col items-center w-full h-full mt-4 mb-4">
 						<img className="cursor-pointer w-10 h-10" src={settings} />
 					</div>
