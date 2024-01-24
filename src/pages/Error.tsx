f537def6dabc5c3a790ec80d36067653675bcc5b
Hoa Do <hoa.do.ma@gmail.com>
2024-01-23 22:39:26 -0500

    fix: empty pages redirect to error page with toolbar

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import StyledBackground from "../layouts/StyledBackground";
+import SidebarLayout from "../layouts/SidebarLayout";
 
 /**
  * Page to handle all routes not accounted for.
@@ -8,19 +8,21 @@ function Error() {
 	const navigate = useNavigate();
 
 	return (
-		<StyledBackground>
+		<SidebarLayout>
 			<div className="flex flex-col items-center justify-center h-screen gap-8">
 				<h1 className="font-semibold text-white text-8xl">404</h1>
-				<h3 className="text-lg text-white">The requested page does not exist.</h3>
+				<h3 className="text-lg text-white">
+					The requested page is either under construction or perhaps does not exist... yet!
+				</h3>
 				<button
-					className=" bg-teal-500 rounded-lg w-[175px] h-[50px] flex justify-center items-center"
+					className=" bg-teal-500 rounded-lg w-[200px] h-[50px] flex justify-center items-center"
 					type="button"
 					onClick={() => navigate("/dashboard")}
 				>
 					<p className="text-lg font-semibold text-white mb-[2px]">Back to Dashboard</p>
 				</button>
 			</div>
-		</StyledBackground>
+		</SidebarLayout>
 	);
 }
 
