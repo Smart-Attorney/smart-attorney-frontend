cb585ffebaaf5648d2f73ff9ffa6f9b67f760b39
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-01-29 15:49:44 -0500

    bob

--- src/pages/CaseFolder.tsx
+++ src/pages/CaseFolder.tsx
@@ -33,7 +33,7 @@ function CaseFolder() {
 	const fileId = useRef<string>("");
 	const fileName = useRef<string>("");
 	const fileUrl = useRef<string>("");
-	const initialDOB = new Date();
+
 	const [caseFolder, setCaseFolder] = useState<CaseFolderObj>({
 		id: "",
 		name: "",
@@ -43,13 +43,16 @@ function CaseFolder() {
 		deadline: "",
 		labels: [],
 		files: [],
-		clientFirstName: "",
-		clientLastName: "",
-		clientSex: "",
-		clientPrimaryLanguage: "",
-		clientCountryOfCitizenship: "",
-		clientDOB: initialDOB,
+		clientInfo: {
+			firstName: "",
+			lastName: "",
+			sex: null,
+			primaryLanguage: "",
+			countryOfCitizenship: "",
+			dateOfBirth: ""
+		} ,
 	});
+	
 	const [caseFiles, setCaseFiles] = useState<CaseFileObj[]>([]);
 	const [uploadModalOpen, setUploadModalOpen] = useState(false);
 	const [fileModalOpen, setFileModalOpen] = useState(false);
