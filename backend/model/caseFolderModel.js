b8c65abd1225a3006973fe116cc2af7f77a9b236
Hoa Do <hoa.do.ma@gmail.com>
2024-02-16 14:37:14 -0500

    remove backend directory now that it exists separately

--- backend/model/caseFolderModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const caseFolderSchema = require('../Schema/caseFolderSchema');
-
-const CaseFolder = mongoose.model('CaseFolder', caseFolderSchema);
-
-module.exports = CaseFolder;
--- backend/model/fileModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const fileSchema = require('../Schema/fileSchema');
-
-const File = mongoose.model('File', fileSchema);
-
-module.exports = File;
