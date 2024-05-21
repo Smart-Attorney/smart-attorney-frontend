49d171f0b0ba6e56879c90ab5b6c2d3802508ed5
Hoa Do <hoa.do.ma@gmail.com>
2024-05-21 18:55:15 -0400

    feat: add background colors for each unique document status

--- src/components/Card/DocumentStatus.tsx
+++ src/components/Card/DocumentStatus.tsx
@@ -4,8 +4,27 @@ interface FileStatusProps {
 }
 
 function DocumentStatus({ id, text }: FileStatusProps) {
+	let statusColor: string;
+	switch (text) {
+		case "In Review":
+			// orange
+			statusColor = "bg-[#ff9c34]";
+			break;
+		case "In Progress":
+			// yellow
+			statusColor = "bg-[#ffd434]";
+			break;
+		case "Submitted":
+			// green
+			statusColor = "bg-[#60bc6c]";
+			break;
+		default:
+			statusColor = "";
+			break;
+	}
+
 	return (
-		<div id={id} className="min-w-max bg-[#53EF0A80] rounded-full px-2.5 py-1">
+		<div id={id} className={`${statusColor} min-w-max rounded-full px-2.5 py-1`}>
 			<p className="text-xs">{text}</p>
 		</div>
 	);
