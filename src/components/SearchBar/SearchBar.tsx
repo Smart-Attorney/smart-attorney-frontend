fdb5f7d2c1a31a3390db30d28636ced88825a743
Hoa Do <hoa.do.ma@gmail.com>
2024-05-28 14:19:12 -0400

    feat: search bar query filters card components by name

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,35 @@
 import { SearchIcon } from "../../assets/smart-attorney-figma/global";
+import { CaseFileObj, DashboardFolderCardObj } from "../../utils/types";
 
-function SearchBar() {
+interface SearchBarProps {
+	cards?: DashboardFolderCardObj[] | CaseFileObj[] | null;
+}
+
+function SearchBar({ cards }: SearchBarProps) {
+	const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value: inputQuery } = event.target;
+		filterCardsByQuery(inputQuery);
+	};
+
+	const filterCardsByQuery = (query: string) => {
+		if (!cards) return;
+
+		if (query.trim().length === 0) {
+			cards.forEach((card) => {
+				document.getElementById(card.id)?.classList.remove("hide");
+			});
+			return;
+		}
+
+		for (let i = 0, n = cards.length; i < n; i++) {
+			if (cards[i].name.toLowerCase().includes(query.toLowerCase().trim())) {
+				document.getElementById(cards[i].id)?.classList.remove("hide");
+			} else {
+				document.getElementById(cards[i].id)?.classList.add("hide");
+			}
+		}
+	};
+
 	return (
 		<div className="flex flex-row justify-between w-full gap-8 pb-8 pl-20 pr-28">
 			<div className="flex flex-row w-full rounded-lg ">
@@ -11,6 +40,7 @@ function SearchBar() {
 					className="w-full pr-3 text-xl font-medium placeholder-black bg-white rounded-r-lg h-11 focus:outline-none"
 					type="search"
 					placeholder="Search..."
+					onChange={handleQueryChange}
 				/>
 			</div>
 
