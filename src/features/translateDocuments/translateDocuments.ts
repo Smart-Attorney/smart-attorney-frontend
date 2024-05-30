68adebe98c66f8073bc304eb3156249380359293
junh4533 <junh4533@gmail.com>
2024-05-30 09:06:31 -0700

    add translated doc uri

--- src/features/translateDocuments/translateDocuments.ts
+++ src/features/translateDocuments/translateDocuments.ts
@@ -1,13 +1,16 @@
 const translateDoc = async (fileName: string): Promise<string> => {
 	const language = "en";
-	const bucketName = process.env.REACT_APP_UPLOAD_BUCKET;
+	const uploadedFilesBucket = process.env.REACT_APP_UPLOAD_BUCKET;
+	const translatedFilesBucket = process.env.REACT_APP_TRANSLATED_BUCKET;
 	const endpoint = process.env.REACT_APP_TRANSLATE_ENDPOINT;
 
-	const inputUri = "gs://" + bucketName + "/" + fileName;
+	const inputUri = "gs://" + uploadedFilesBucket + "/" + fileName;
+	const outputUriPrefix = "gs://" + translatedFilesBucket + "/";
 	const translatedDocPath = process.env.REACT_APP_TRANSLATED_DOCS;
 
 	const body = {
 		inputUri: inputUri,
+		outputUriPrefix: outputUriPrefix,
 	};
 
 	const response = await fetch(endpoint as string, {
@@ -22,7 +25,7 @@ const translateDoc = async (fileName: string): Promise<string> => {
 
 	if (response.status === 200) {
 		const noExtensionFileName = fileName.replaceAll(".pdf", "");
-		const translatedDoc = `${translatedDocPath}/${bucketName}_${noExtensionFileName}_${language}_translations.pdf`;
+		const translatedDoc = `${translatedDocPath}/${uploadedFilesBucket}_${noExtensionFileName}_${language}_translations.pdf`;
 
 		return Promise.resolve(translatedDoc);
 	} else return Promise.reject(res.error);
