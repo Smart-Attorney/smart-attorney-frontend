91b7a9d2c176fa7106a197a43fe8c9fa966013de
Hoa Do <hoa.do.ma@gmail.com>
2024-07-17 18:55:25 -0400

    update mock user to use user_auth

--- src/services/local-database/mock-user.ts
+++ src/services/local-database/mock-user.ts
@@ -1,23 +1,30 @@
+import { UserAuthEntity, UserEntity } from "./entities";
 import { SqlTables } from "./sql-tables";
-import { UserEntity } from "./entities";
 
 export class MockUser {
-	private static mockUser: UserEntity = Object.freeze({
-		user_id: "12345abcdeABCDE0",
+	private static mockUser: UserEntity = {
+		user_id: "12345abcdeABCDE0000",
 		first_name: "Jane",
 		last_name: "Doe",
 		firm_name: "Attorney Law Group",
 		company_email: "lawyer@email.com",
 		password: "password123",
-	});
+	};
 
-	private static set(): void {
+	private static mockUserAuth: UserAuthEntity = {
+		user_auth_id: "12345abcdeABCDE1111",
+		company_email: "lawyer@email.com",
+		salt: "$2a$15$1SbYDpXwQ6r904RSwsgQsO",
+		password_hash: "$2a$15$1SbYDpXwQ6r904RSwsgQsOXojpQfc/xDY12nEc9/FvzWAXDx3CM9W",
+	};
+
+	private static setMockUser(): void {
 		const userTable: UserEntity[] = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER) as string);
 		const updatedUserTable: UserEntity[] = [this.mockUser, ...userTable];
 		localStorage.setItem(SqlTables.TABLE.USER, JSON.stringify(updatedUserTable));
 	}
 
-	private static exists(): boolean {
+	private static doesMockUserExist(): boolean {
 		const userTable: UserEntity[] = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER) as string);
 		const mockUser = JSON.stringify(this.mockUser);
 		for (let i = 0, n = userTable.length; i < n; i++) {
@@ -29,16 +36,15 @@ export class MockUser {
 		return false;
 	}
 
-	private static removeDeprecated(): void {
-		const uniqueUsersArray = [];
-		const mockUserArray = [];
-		const userArray = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER) as string);
+	private static removeMockUsers(): void {
+		const uniqueUsersArray: UserEntity[] = [];
+		const mockUserArray: UserEntity[] = [];
+		const userArray: UserEntity[] = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER) as string);
 		for (let i = 0, n = userArray.length; i < n; i++) {
 			const firstNameMatch = userArray[i].first_name === this.mockUser.first_name;
 			const lastNameMatch = userArray[i].last_name === this.mockUser.last_name;
 			const companyEmailMatch = userArray[i].company_email === this.mockUser.company_email;
-			const passwordMatch = userArray[i].password === this.mockUser.password;
-			if (firstNameMatch && lastNameMatch && companyEmailMatch && passwordMatch) {
+			if (firstNameMatch && lastNameMatch && companyEmailMatch) {
 				mockUserArray.push(userArray[i]);
 			} else {
 				uniqueUsersArray.push(userArray[i]);
@@ -48,9 +54,50 @@ export class MockUser {
 		localStorage.setItem(SqlTables.TABLE.USER, JSON.stringify(updatedArray));
 	}
 
+	private static setMockUserAuth(): void {
+		const userAuthTable: UserAuthEntity[] = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER_AUTH) as string);
+		const updatedUserAuthTable: UserAuthEntity[] = [this.mockUserAuth, ...userAuthTable];
+		localStorage.setItem(SqlTables.TABLE.USER_AUTH, JSON.stringify(updatedUserAuthTable));
+	}
+
+	private static doesMockUserAuthExist(): boolean {
+		const userAuthTable: UserAuthEntity[] = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER_AUTH) as string);
+		const mockUserAuth = JSON.stringify(this.mockUserAuth);
+		for (let i = 0, n = userAuthTable.length; i < n; i++) {
+			const userAuthInTable = JSON.stringify(userAuthTable[i]);
+			if (mockUserAuth === userAuthInTable) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static removeMockUserAuths(): void {
+		const uniqueUserAuthsArray: UserAuthEntity[] = [];
+		const mockUserAuthArray: UserAuthEntity[] = [];
+		const userAuthArray: UserAuthEntity[] = JSON.parse(localStorage.getItem(SqlTables.TABLE.USER_AUTH) as string);
+		for (let i = 0, n = userAuthArray.length; i < n; i++) {
+			const companyEmailMatch = userAuthArray[i].company_email === this.mockUserAuth.company_email;
+			const saltMatch = userAuthArray[i].salt === this.mockUserAuth.salt;
+			const passwordHashMatch = userAuthArray[i].password_hash === this.mockUserAuth.password_hash;
+			if (companyEmailMatch && saltMatch && passwordHashMatch) {
+				mockUserAuthArray.push(userAuthArray[i]);
+			} else {
+				uniqueUserAuthsArray.push(userAuthArray[i]);
+			}
+		}
+		const updatedArray = [...uniqueUserAuthsArray];
+		localStorage.setItem(SqlTables.TABLE.USER_AUTH, JSON.stringify(updatedArray));
+	}
+
 	public static create() {
-		if (this.exists()) return;
-		this.removeDeprecated();
-		this.set();
+		if (!this.doesMockUserExist()) {
+			this.removeMockUsers();
+			this.setMockUser();
+		}
+		if (!this.doesMockUserAuthExist()) {
+			this.removeMockUserAuths();
+			this.setMockUserAuth();
+		}
 	}
 }
