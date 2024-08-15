7b8c1b21ad00ccdf649e309e29213eb321880d40
Hoa Do <hoa.do.ma@gmail.com>
2024-08-15 15:50:41 -0400

    fix: wrong endpoint for retrieving all user case labels

--- src/components/SortBar/api/get-case-labels.tsx
+++ src/components/SortBar/api/get-case-labels.tsx
@@ -4,13 +4,13 @@ import { MockRequest } from "../../../lib/mock-request";
 import { CaseLabelController } from "../../../services/local-backend/case-label/case-label-controller";
 
 const mockApi = async (): Promise<Response> => {
-	const endpoint = `/users/cases`;
+	const endpoint = `/users/case-labels`;
 	const request = new MockRequest().get(endpoint);
 	return await new CaseLabelController().getAllUserCaseLabelsHandler(request);
 };
 
 const fetchApi = async (): Promise<Response> => {
-	const endpoint = `/users/cases`;
+	const endpoint = `/users/case-labels`;
 	return await new FetchWrapper().get(endpoint);
 };
 
