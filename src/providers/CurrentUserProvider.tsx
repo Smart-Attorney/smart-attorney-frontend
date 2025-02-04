d4116d1f0ba3089ca6faad3120fa553ce4a37192
Hoa Do <hoa.do.ma@gmail.com>
2025-02-04 16:41:37 -0500

    refactor: update to use local storage instead of session storage

--- src/providers/CurrentUserProvider.tsx
+++ src/providers/CurrentUserProvider.tsx
@@ -17,11 +17,11 @@ export function CurrentUserProvider({ children }: { children: React.ReactNode })
 	const CURRENT_USER_KEY = "current_user";
 
 	const getCurrentUser = (): CurrentUser => {
-		return JSON.parse(sessionStorage.getItem(CURRENT_USER_KEY) as string);
+		return JSON.parse(localStorage.getItem(CURRENT_USER_KEY) as string);
 	};
 
 	const setCurrentUser = (user: CurrentUser): void => {
-		sessionStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
+		localStorage.setItem(CURRENT_USER_KEY, JSON.stringify(user));
 	};
 
 	return (
